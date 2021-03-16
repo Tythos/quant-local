4a3a4937f0ee9f268433d8910d29447ad6c2b41b
Brian Kirkpatrick <code@tythos.net>
2021-03-15 17:07:29 -0700

    oh, hey, working reports

--- __init__.py
+++ __init__.py
@@ -3,10 +3,11 @@
 
 import os
 import re
+import csv
 import math
 import pprint
-import csv
 import xlrd
+import numpy
 
 MOD_PATH, _ = os.path.split(os.path.abspath(__file__))
 DATASTORE_PATH = MOD_PATH + "/datastore"
@@ -21,7 +22,7 @@ def getDatePaths():
             candidates.append(os.path.abspath(DATASTORE_PATH + "/%s" % folderName))
     return candidates
 
-def getSectors():
+def getSectorsTable():
     """Returns dictionary entries in the Sectors table
     """
     sectorsPath = DATASTORE_PATH + "/sectors.csv"
@@ -35,7 +36,7 @@ def getSectorPaths(datePath):
        the given date
     """
     candidates = []
-    sectors = getSectors()
+    sectors = getSectorsTable()
     pattern = "^(%s)\.xls$" % "|".join([sector["Code"] for sector in sectors])
     for fileName in os.listdir(datePath):
         if re.match(pattern, fileName):
@@ -60,40 +61,45 @@ class Filter(object):
            Three comparison types are supported: string, numeric, and dollar.
            Dollar values are encoded as strings but begin with the "$"
            character and may be suffixed by a "k", "M", or "B" magnitude. These
-           are cast to numeric values before a comparison is made.
+           are cast to numeric values before a comparison is made. If an error
+           occurs, the property is likely ill-formed and the filter is
+           therefore rejected.
         """
-        lhs = symbProps[self.property]
-        if type(lhs) is type("") and lhs[0] == "$":
-            # convert to numerical dollar amount
-            suffix = lhs[-1]
-            if not re.match(r"^\d$", suffix):
-                lhs = lhs[1:-1]
-                mag = 0
-                if suffix == "k":
-                    mag = 3
-                elif suffix == "M":
-                    mag = 6
-                elif suffix == "B":
-                    mag = 9
-                else:
-                    raise Exception("Unsupported financial magnitude suffix '%s'" % suffix)
-                lhs = float(lhs) * math.pow(10, mag)
-        if type(self.value) is type(0.0):
-            lhs = float(lhs)
-        if self.comparator == "<":
-            return self.compareLT(lhs, self.value)
-        elif self.comparator == "<=":
-            return self.compareLE(lhs, self.value)
-        elif self.comparator == "==":
-            return self.compareEQ(lhs, self.value)
-        elif self.comparator == "!=":
-            return not self.compareEQ(lhs, self.value)
-        elif self.comparator == ">=":
-            return self.compareGE(lhs, self.value)
-        elif self.comparator == ">":
-            return self.compareGT(lhs, self.value)
-        else:
-            raise Exception("Invalid comparator '%s'" % self.comparator)
+        try:
+            lhs = symbProps[self.property]
+            if type(lhs) is type("") and 0 < len(lhs) and lhs[0] == "$":
+                # convert to numerical dollar amount
+                suffix = lhs[-1]
+                if not re.match(r"^\d$", suffix):
+                    lhs = lhs[1:-1]
+                    mag = 0
+                    if suffix == "k":
+                        mag = 3
+                    elif suffix == "M":
+                        mag = 6
+                    elif suffix == "B":
+                        mag = 9
+                    else:
+                        raise Exception("Unsupported financial magnitude suffix '%s'" % suffix)
+                    lhs = float(lhs) * math.pow(10, mag)
+            if type(self.value) is type(0.0):
+                lhs = float(lhs)
+            if self.comparator == "<":
+                return self.compareLT(lhs, self.value)
+            elif self.comparator == "<=":
+                return self.compareLE(lhs, self.value)
+            elif self.comparator == "==":
+                return self.compareEQ(lhs, self.value)
+            elif self.comparator == "!=":
+                return not self.compareEQ(lhs, self.value)
+            elif self.comparator == ">=":
+                return self.compareGE(lhs, self.value)
+            elif self.comparator == ">":
+                return self.compareGT(lhs, self.value)
+            else:
+                raise Exception("Invalid comparator '%s'" % self.comparator)
+        except Exception:
+            return False
 
     def compareLT(self, lhs, rhs):
         """For inequality this is asserted to be numeric-only lesser-than
@@ -145,7 +151,15 @@ class Sector(object):
     def __init__(self, xlsPath):
         """
         """
+        self.sectorPath = xlsPath
         self.wb = xlrd.open_workbook(xlsPath)
+
+    def getCode(self):
+        """
+        """
+        _, sectorFile = os.path.split(self.sectorPath)
+        sectorCode, _ = os.path.splitext(sectorFile)
+        return sectorCode
 
     def getSymbols(self):
         """Returns list of all symbols listed for this sector
@@ -163,7 +177,7 @@ class Sector(object):
                 symbols.append(row)
         return symbols
 
-    def getSymbol(self, symbol):
+    def getSecurity(self, symbol):
         """Aggregates all symbol properties across all worksheets
         """
         sheet_names = self.wb.sheet_names()
@@ -180,29 +194,89 @@ class Sector(object):
                 properties[header[i]] = values[i]
         return properties
 
-def main():
+def getSectors():
     """
     """
     datePaths = getDatePaths()
     sectorPaths = getSectorPaths(datePaths[0])
-    sector = Sector(sectorPaths[3])
-    symbols = sector.getSymbols()
-    symbol = sector.getSymbol(symbols[19])
-    filters = [
+    return [Sector(sectorPath) for sectorPath in sectorPaths]
+
+def filterSymbols(sectors, filters):
+    """
+    """
+    allPassed = {}
+    for sector in sectors:
+        code = sector.getCode()
+        secPassed = []
+        symbols = sector.getSymbols()
+        for symbol in symbols:
+            security = sector.getSecurity(symbol)
+            for ndx, fltr in enumerate(filters):
+                if not fltr.isOkay(security):
+                    break
+                elif ndx == len(filters) - 1:
+                    secPassed.append(symbol)
+        if 0 < len(secPassed):
+            allPassed[code] = secPassed
+    return allPassed
+
+def getFilters():
+    """
+    """
+    return [
         Filter("Company Headquarters Location", "==", "United States of America"),
         Filter("Equity Summary Score from StarMine from Refinitiv", "==", "Very Bullish"),
         Filter("Security Price", ">", 10.0),
         Filter("Security Price", "<", 100.0),
-        Filter("Market Capitalization", ">=", 1e9)
+        Filter("Market Capitalization", ">=", 1e9),
+        Filter("Price Performance (52 Weeks)", ">", 0.0)
     ]
-    pprint.pprint(symbol)
-    for ndx, fltr in enumerate(filters):
-        print("Evaluating %s" % fltr.property)
-        if not fltr.isOkay(symbol):
-            print("Does not pass")
-            break
-        elif ndx == len(filters) - 1:
-            print("It passed")
+
+def getMetrics(sector, symbols):
+    """Returns a 2xn numpy.Array of metrics for the given symbols from the
+       given sector.
+    """
+    metrics = [ # hard-coded for now, could easily be parameterized
+        "Price Performance (52 Weeks)",
+        "Standard Deviation (1 Yr Annualized)"
+    ]
+    table = numpy.zeros((2, len(symbols)))
+    for i, metric in enumerate(metrics):
+        for j, symbol in enumerate(symbols):
+            security = sector.getSecurity(symbol)
+            table[i,j] = security[metric]
+    return table
+
+def getFrontier(x, y):
+    """Given x and y numpy.array objects (of matching 1d length), returns
+       indices of a subset of points that constitute the frontier (in this
+       case: max x, min y). Ordered in increasing y/x value.
+    """
+    order = numpy.argsort(x)[-1::-1]
+    indices = [order[0]]
+    y_ = y[indices[0]]
+    for i in order[1:]:
+        if y[i] < y_:
+            indices.append(i)
+            y_ = y[i]
+    return indices[-1::-1]
+
+def main():
+    """
+    """
+    sectors = getSectors()
+    filters = getFilters()
+    allPassed = filterSymbols(sectors, filters)
+    for sector in sectors:
+        code = sector.getCode()
+        if code not in allPassed:
+            continue
+        symbols = allPassed[code]
+        xy = getMetrics(sector, symbols)
+        frontier = getFrontier(xy[0,:], xy[1,:])
+        if 1 < len(frontier):
+            ndx = frontier[-2] # second-highest on frontier
+            print("Recommended buy for sector %s: %s" % (sector.getCode(), symbols[ndx]))
 
 if __name__ == "__main__":
     main()
